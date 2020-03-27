9f8ca0300008cd14275f4b65d96e9a40dbb7a9e0
Technology-Geek <61490932+Technology-Geek@users.noreply.github.com>
2020-03-27 21:19:52 +0300

    Updates

---
+++ precache-manifest.3567db5e91b6ba675e703ef0bff4cca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3e33c0b06b0c3c92c359453ce23c6c",
+    "url": "/Todo_App_React/index.html"
+  },
+  {
+    "revision": "437aeee5a4716860f8a2",
+    "url": "/Todo_App_React/static/js/2.f8d69b25.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo_App_React/static/js/2.f8d69b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10479e3eeb48309f788",
+    "url": "/Todo_App_React/static/js/main.f983e4a9.chunk.js"
+  },
+  {
+    "revision": "ffefbf35c69823152784",
+    "url": "/Todo_App_React/static/js/runtime-main.7ba70872.js"
+  }
+]);
